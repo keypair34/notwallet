43d5f8910ff35d1d2e5ea7ccea52372bd4114623
Seto Elkahfi <setoelkahfi@gmail.com>
2025-08-12 13:09:03 +0200

    Replace mock activity list with Solscan integration

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -124,7 +124,7 @@ export default function WalletHome() {
                 setShowSwitchModal(true);
               }}
             />
-            <ActivityCard />
+            <ActivityCard wallet={wallet} />
           </Box>
           <ActiveKeypairSelectionModal
             open={showSwitchModal}
